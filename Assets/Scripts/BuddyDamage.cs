57bc3585ffc77389671aebbb53666f8f96730671
Yoneo Arai <yoneo@berkeley.edu>
2018-04-17 18:44:49 -0700

    Adding force damage

---
+++ Assets/Scripts/BuddyDamage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuddyDamage : MonoBehaviour {
+
+	// Use this for initialization
+	GameObject buddy;
+	string[] bodyParts;
+
+	GameObject self;
+
+	void Start () {
+		buddy = GameObject.Find("Buddy");
+		bodyParts = new string[]{"Head", "Torso", "HandR", "HandL", "FootR", "FootL"};
+		self = this.gameObject;
+	}
+
+	void OnCollisionEnter(Collision col) {
+		float notBodyOnBody = System.Array.IndexOf(bodyParts, col.gameObject.name);
+		if (notBodyOnBody == -1) {
+			float damage = col.relativeVelocity.magnitude;
+			print(col.gameObject.name + " : " + damage);
+
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
